d9e2200263ea97839353d590217c2a61289c45d3
Stephane <stephanefokyin@gmail.com>
2020-06-14 10:19:54 +0200

    added collection preview and collection items components

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Kiwi-e-comm based on Complete React developer 2020 course
+# Kiwi-e-comm based on Complete React developer 2020 course
--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 
-const HatsPage = () => (
-  <div>
-    <h1>HATSPAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
